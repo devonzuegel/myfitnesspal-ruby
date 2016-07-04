243681b3b852d219d19d87461e34f55354280eee
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-07-03 17:14:11 -0700

    Add HttpRequestParams
    Why:
    
    * MyFitnessPal iPhone API endpoint requires data to be sent to it in
      a specific format with certain headers
    
    This change addresses the need by:
    
    * The new HttpRequestParams class construct headers and a POST body
      given a encoded sync API request.

---
+++ http_request_params.rb
@@ -0,0 +1,35 @@
+require 'concord'
+
+# Given a encoded sync API request, construct headers and a POST body.
+class HttpRequestParams
+  include Concord.new(:data)
+
+  def body
+    <<~HEREDOC
+    --#{mime_boundary}\r\n
+    Content-Disposition: form-data; name="syncdata"; filename="syncdata.dat"\r\n
+    Content-Type: application/octet-stream\r\n
+    \r\n
+    #{data}
+    \r\n
+    --#{mime_boundary}--\r\n
+    HEREDOC
+  end
+
+  def headers
+    {
+      'User-Agent'     => USER_AGENT,
+      'Content-Type'   => "multipart/form-data; boundary=#{mime_boundary}",
+      'Content-Length' => body.length
+    }
+  end
+
+  private
+
+  URL        = 'https://www.myfitnesspal.com/iphone_api/synchronize'
+  USER_AGENT = 'Dalvik/1.6.0 (Linux; U; Android 4.4.2; sdk Build/KK)'
+
+  def mime_boundary
+    @mime_boundary ||= 78.times.map { [*('a'..'z')].sample }.join
+  end
+end
--- spike.rb
+++ spike.rb
@@ -1,1 +1,7 @@
-puts "#{}"
+require 'securerandom'
+
+username          = ENV['MYFITNESSPAL_USERNAME']
+password          = ENV['MYFITNESSPAL_PASSWORD']
+installation_uuid = SecureRandom.uuid
+
+p installation_uuid
