a3f739fdb235436794e03640974f09a48ac0e51c
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-07-08 15:24:55 -0700

    Add spec for parsing response fixture

--- spec/spike_spec.rb
+++ spec/spike_spec.rb
@@ -3,22 +3,23 @@ require 'codec'
 
 RSpec.describe Spike do
   let(:response_fixture) do
-    Pathname.new(__dir__)
-      .join('fixtures/response.bin')
-      .expand_path
-      .read.b
+    Pathname.new(__dir__).join('fixtures/response.bin').expand_path.read.b
   end
 
   it '...' do
-    # expect do
-      Codec.new(response_fixture).read_packets do |p|
-        type = p.packet_type
-        puts "Type #{type}: #{Binary::Type.supported_types.fetch(type)}".black
-
-        # puts '---------------------------'.black; ap p.to_h
-      end
-    # end
-    # .to raise_error NotImplementedError, "Type 4 is not supported"
+    packet_types = Hash.new(0)
+    Codec.new(response_fixture).read_packets do |p|
+      packet_types[Binary::Type.supported_types.fetch(p.packet_type)] += 1
+    end
+    expect(packet_types).to eq(
+      Binary::Exercise           => 1,
+      Binary::Food               => 54,
+      Binary::FoodEntry          => 848,
+      Binary::MealIngredients    => 10,
+      Binary::MeasurementTypes   => 1,
+      Binary::SyncResponse       => 1,
+      Binary::UserPropertyUpdate => 1
+    )
   end
 
   it 'Request should retrieve expected response (SKIPPED BY DEFAULT)', :skip do
