be0ada3fb56f72deaadc79989ddb3076171ead48
John Backus <johncbackus@gmail.com>
2016-07-16 23:52:55 -0700

    Refactor struct implementation
      - Namespace under MFP so we don't conflict with ::Struct
    
      - Add a test and kill a few mutations
    
      - Use described_class in spec

--- lib/struct.rb
+++ lib/struct.rb
@@ -1,54 +1,53 @@
 # Interpret strings as packed binary data.
-class Struct
-  def self.parse(str, num_bytes, pack_directive)
-    bytes = str.slice(0, num_bytes)
-    rest  = str.slice(num_bytes, str.length)
+module MFP
+  module Struct
+    def self.parse(str, num_bytes, pack_directive)
+      fail EOFError if str.length < num_bytes
 
-    fail EOFError if bytes.length < num_bytes
+      unpacked, = str.unpack(pack_directive)
+      rest      = str.slice(num_bytes, str.length)
 
-    [
-      *bytes.unpack(pack_directive),
-      rest
-    ]
-  end
-
-  def self.pack_short(val)
-    [val].pack('s>')
-  end
-
-  def self.pack_long(val)
-    [val].pack('l>')
-  end
-
-  def self.pack_string(str)
-    [
-      [str.length].pack('s>'),
-      str
-    ].join
-  end
-
-  def self.pack_hash(hash, key_type: 'short')
-    result = [
-      [hash.keys.length].pack('s>')
-    ]
-
-
-    hash.each do |key, value|
-      result += [
-        pack_method(key_type).call(key),
-        pack_string(value)
-      ]
+      [unpacked, rest]
     end
 
-    result.join
-  end
+    def self.pack_short(val)
+      [val].pack('s>')
+    end
 
-  def self.pack_method(key_type)
-    case key_type.downcase
-      when 'short'  then -> (val) { pack_short(val)  }
-      when 'long'   then -> (val) { pack_long(val)   }
-      when 'string' then -> (val) { pack_string(val) }
-      else fail NotImplementedError
+    def self.pack_long(val)
+      [val].pack('l>')
+    end
+
+    def self.pack_string(str)
+      [
+        [str.length].pack('s>'),
+        str
+      ].join
+    end
+
+    def self.pack_hash(hash, key_type: 'short')
+      result = [
+        [hash.length].pack('s>')
+      ]
+
+      hash.each do |key, value|
+        result += [
+          pack_method(key_type).call(key),
+          pack_string(value)
+        ]
+      end
+
+      result.join
+    end
+
+    def self.pack_method(key_type)
+      case key_type
+        when 'short'  then -> (val) { pack_short(val)  }
+        when 'long'   then -> (val) { pack_long(val)   }
+        when 'string' then -> (val) { pack_string(val) }
+        else fail NotImplementedError
+      end
     end
   end
 end
+
