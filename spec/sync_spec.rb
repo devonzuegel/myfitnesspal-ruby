a28122baf1938c39f20c4f6e37a847b63b18bad1
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-07-18 00:35:32 -0700

    Clean up dead code and manage namespacing.
    - Remove String extension. This was dead code, and
      it was an unecessary monkey patch.
    - Namespace HttpRequestParams under MFP.

--- spec/sync_spec.rb
+++ spec/sync_spec.rb
@@ -1,16 +1,16 @@
 require 'sync'
 
 RSpec.describe MFP::Sync do
-  it 'retrieves expected response' do
-    expect(ENV['MYFITNESSPAL_USERNAME']).to_not eq nil
-    expect(ENV['MYFITNESSPAL_PASSWORD']).to_not eq nil
+  let(:sync) do
+    described_class.new(ENV['MYFITNESSPAL_USERNAME'], ENV['MYFITNESSPAL_PASSWORD'])
+  end
 
-    sync = described_class.new(ENV['MYFITNESSPAL_USERNAME'], ENV['MYFITNESSPAL_PASSWORD'])
+  it 'retrieves expected response' do
     expect(sync.response.status).to eq 200
     expect(sync.response.body.to_s.length).to be >= 213_000
   end
 
   it 'should sync all pages, not just the first' do
-    skip
+
   end
 end
