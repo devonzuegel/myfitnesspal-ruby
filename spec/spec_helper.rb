5d4892c44baa9c00562d56432c71bc2818e8cf06
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-08-12 19:05:21 -0700

    Add web-mocked specs for multi-paginated sync

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ require 'bundler/setup'
 require 'rspec'
 require 'awesome_print'
 require 'colorize'
+require 'webmock/rspec'
 
 RSpec.configure do |config|
   config.filter_run_excluding skip: true
@@ -10,6 +11,8 @@ RSpec.configure do |config|
   # nothing is tagged with `:focus`, all examples get run.
   config.filter_run focus: ENV['CI'] != 'true'
   config.run_all_when_everything_filtered = true
+
+  WebMock.disable_net_connect!(allow_localhost: true)
 
   config.around(:each) do |test|
     Timeout.timeout(320, &test)
