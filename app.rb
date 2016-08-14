c39d152b08220517416630940197eb8842e1ca43
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-08-14 15:38:55 -0700

    Add Sinatra base app with Thin server

---
+++ app.rb
@@ -0,0 +1,21 @@
+require 'rubygems'
+require 'bundler'
+
+Bundler.require
+
+module API
+  class App < Sinatra::Application
+    configure do
+      disable :method_override
+      disable :static
+
+      set :sessions,
+          httponly:     true,
+          secure:       production?,
+          expire_after: 31_557_600, # 1 year
+          secret:       ENV['SESSION_SECRET']
+    end
+
+    use Rack::Deflater
+  end
+end
