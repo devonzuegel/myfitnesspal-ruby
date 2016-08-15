ff5e9daeb98ed1d4c356c48c5bf27e87f990bc5a
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-08-15 13:31:49 -0700

    Add db rollback after each spec

--- spec/api/models/user_spec.rb
+++ spec/api/models/user_spec.rb
@@ -3,9 +3,7 @@ RSpec.describe API::Models::User do
   let(:password) { 'pword' }
 
   it '..' do
-    DB[:users].where(username: /.*/).delete
     described_class.create(username: username, password: password)
     expect(DB[:users].count).to eql 1
-    DB[:users].where(username: /.*/).delete
   end
 end
