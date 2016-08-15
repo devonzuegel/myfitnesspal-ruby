1831310fbe873daccab3c63a319dfa3ca9e23de8
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-08-15 13:35:39 -0700

    Add user specs

--- api/models/user.rb
+++ api/models/user.rb
@@ -1,6 +1,13 @@
 module API
   module Models
     class User < Sequel::Model
+      plugin :validation_helpers
+
+      def validate
+        super
+        validates_presence %i[username password]
+        validates_unique :username
+      end
       # Raise error if user with given username is already in system
       # Create user with username, password
       # Start background job to sync user's history
