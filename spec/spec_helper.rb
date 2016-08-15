4eb318f4fbac09b23b27f9fa49f61c9f09353c95
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-08-15 13:31:49 -0700

    Add db rollback after each spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,20 +6,23 @@ DB = Sequel.connect('postgres://devonzuegel@localhost/mfp_api_test')
 
 require Pathname.new('.').join('app').expand_path
 
-RSpec.configure do |config|
-  config.filter_run_excluding skip: true
+RSpec.configure do |c|
+  c.filter_run_excluding skip: true
 
   # Tag examples with `:focus` to run them individually. When
   # nothing is tagged with `:focus`, all examples get run.
-  config.filter_run focus: ENV['CI'] != 'true'
-  config.run_all_when_everything_filtered = true
+  c.filter_run focus: ENV['CI'] != 'true'
+  c.run_all_when_everything_filtered = true
 
   # Disable external requests
   WebMock.disable_net_connect!(allow_localhost: true)
 
   # Fail tests that run for longer than 1 seconds
-  config.around(:each) { |test| Timeout.timeout(1, &test) }
+  c.around(:each) { |t| Timeout.timeout(1, &t) }
+
+  # Rollback db transactions after each test
+  c.around(:each) { |t| DB.transaction(rollback: :always, auto_savepoint: true) { t.run } }
 
   # Randomize order of specs
-  config.order = 'random'
+  c.order = 'random'
 end
