c2d3647f334a3d842a068abdeecc89da876ff1ae
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-08-15 12:35:51 -0700

    Add rspec scaffolding for models, database specs

---
+++ spec/api/models/user_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe API::Models::User do
+  let(:username) { 'uname' }
+  let(:password) { 'pword' }
+
+  it '..' do
+    skip
+    # expect(described_class.create(username, password)).to eql nil
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,10 @@
 require 'bundler/setup'
-require 'rspec'
-require 'awesome_print'
-require 'colorize'
-require 'webmock/rspec'
+Bundler.require
 
-require_relative '../app'
-# Dir.glob(Pathname.new('..').join('api', '**', '*.rb').expand_path) { |f| require f }
+ENV['RACK_ENV'] = 'test'
+DB = Sequel.connect('postgres://devonzuegel@localhost/mfp_api_test')
+
+require Pathname.new('.').join('app').expand_path
 
 RSpec.configure do |config|
   config.filter_run_excluding skip: true
@@ -15,9 +14,12 @@ RSpec.configure do |config|
   config.filter_run focus: ENV['CI'] != 'true'
   config.run_all_when_everything_filtered = true
 
+  # Disable external requests
   WebMock.disable_net_connect!(allow_localhost: true)
 
-  config.around(:each) do |test|
-    Timeout.timeout(320, &test)
-  end
+  # Fail tests that run for longer than 1 seconds
+  config.around(:each) { |test| Timeout.timeout(1, &test) }
+
+  # Randomize order of specs
+  config.order = 'random'
 end
