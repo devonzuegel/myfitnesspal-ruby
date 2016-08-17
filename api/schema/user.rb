73de84ec5c3102e4b7a0e05d746aba05da574d1d
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-08-17 12:05:47 -0700

    Rename User schema to User::Creation

--- api/schema/user.rb
+++ api/schema/user.rb
@@ -1,9 +1,11 @@
 module API
   module Schema
-    User =
-      Dry::Validation.Schema do
-        required(:username).filled(:str?, size?: 4..30)
-        required(:password).filled(:str?, size?: 6..255)
-      end
+    module User
+      Creation =
+        Dry::Validation.Schema do
+          required(:username).filled(:str?, size?: 4..30)
+          required(:password).filled(:str?, size?: 6..255)
+        end
+    end
   end
 end
