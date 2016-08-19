13873e3940e57b5f4d21edfd94e34aff400db548
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-08-19 15:47:26 -0700

    Add Procto to Gemfile

---
+++ db/migrate/002_create_users_table.rb
@@ -0,0 +1,26 @@
+Sequel.migration do
+  NO_SPACES = /^\S*$/
+
+  up do
+    extension(:constraint_validations)
+    create_table(:users) do
+      primary_key :id
+
+      String :username, null: false, unique: true
+      String :password, null: false
+
+      validate do
+        length_range 4..30, :username
+        format NO_SPACES, :username
+
+        length_range 6..255, :password
+        format NO_SPACES, :password
+      end
+    end
+  end
+
+  down do
+    extension(:constraint_validations)
+    drop_table(:users)
+  end
+end
