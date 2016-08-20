5f0c45367076f99ae8fa3157acdb75f75cdb11cc
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-08-20 03:15:07 -0700

    Progress on Routes::Users

--- api/routes/base.rb
+++ api/routes/base.rb
@@ -1,6 +1,22 @@
 module API
   module Routes
     class Base < Sinatra::Application
+      # def self.registered(app)
+      #   puts '-------------------'.yellow
+      #   puts '-------------------'.yellow
+      #   puts '-------------------'.yellow
+      #   puts '-------------------'.yellow
+      #   puts '-------------------'.yellow
+      # end
+
+      # private
+
+      def symbolize_keys(myhash)
+        myhash.keys.each do |key|
+          myhash[(key.to_sym rescue key) || key] = myhash.delete(key)
+        end
+      end
+
       def json(data)
         Rack::Response.new(
           data.to_json,
--- spec/api/routes/users_spec.rb
+++ spec/api/routes/users_spec.rb
@@ -1,12 +1,30 @@
-require 'spec_helper'
-
 describe API::Routes::Users do
   include Rack::Test::Methods
 
-  let(:app) { described_class }
+  let(:app)          { described_class.new('abcdefg 123123123') }
+  let(:valid_params) { { 'username' => 'devon', 'password' => 'x' * 6 } }
+  let(:missing_keys_error) do
+    {
+      'errors' => {
+        'password' => ['is missing', 'size must be within 6 - 255'],
+        'username' => ['is missing', 'size must be within 4 - 30']
+      }
+    }
+  end
 
-  it 'should require the expected keys' # do
-  #   get '/users/create'
-  #   ap JSON.parse(last_response.body)
-  # end
+  describe 'get /users/create' do
+    it 'should require the expected keys' do
+      get '/users/create'
+      expect(JSON.parse(last_response.body))
+        .to eql(missing_keys_error)
+    end
+
+    it 'returns the successfully created user' do
+      get '/users/create', valid_params
+      expect(JSON.parse(last_response.body))
+        .to eql(valid_params)
+    end
+
+    it 'creates a new user in the repository'
+  end
 end
