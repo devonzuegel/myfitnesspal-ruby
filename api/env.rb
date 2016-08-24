3a358241bb7eb8fea12506e7c12841ecf15c7f63
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-08-24 14:59:30 -0700

    Add #to_s to Env

--- api/env.rb
+++ api/env.rb
@@ -3,5 +3,11 @@ module API
     include Anima.new(:repository)
 
     attr_reader :repository
+
+    def to_h
+      {
+        repository: repository.to_s
+      }
+    end
   end
 end
--- spec/api/env_spec.rb
+++ spec/api/env_spec.rb
@@ -1,8 +1,15 @@
 describe API::Env do
-  let(:repository) { instance_double(API::SqlRepo, db: Sequel.mock(host: 'postgres')) }
+  let(:db)         { Sequel.mock(host: 'postgres') }
+  let(:repository) { instance_double(API::SqlRepo, db: db, to_s: 'mockuri') }
   let(:env)        { described_class.new(repository: repository) }
 
   it 'initializes with a repository' do
     expect(env.repository).to eql repository
+  end
+
+  describe '#to_h' do
+    it 'provides a string representation of the environment' do
+      expect(env.to_h).to eql(repository: 'mockuri')
+    end
   end
 end
