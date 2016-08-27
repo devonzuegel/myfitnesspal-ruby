cd34630ae7ba2d0e08735dcce95f7ea8ebeb1897
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-08-26 17:38:57 -0700

    Add route to /favicon.ico

--- api/app.rb
+++ api/app.rb
@@ -1,12 +1,15 @@
-Dir.glob(Pathname.new('.').join('api', '**', '*.rb').expand_path) { |f| require f }
-
 module API
+  ROOT = Pathname.new('.').expand_path
+
+  Dir.glob(ROOT.join('api', '**', '*.rb')) { |f| require f }
+
   module Models
     autoload :User, 'models/user'
   end
 
   class App < Routes::Base
     get '/favicon.ico' do
+      send_file(ROOT.join('public', 'favicon.ico'))
     end
 
     use Routes::Users
