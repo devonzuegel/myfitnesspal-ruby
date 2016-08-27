9a994077368560629fa94a0dbf1d9635574c8428
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-08-26 21:32:15 -0700

    Add favicon to environment

--- api/app.rb
+++ api/app.rb
@@ -1,6 +1,5 @@
 module API
   ROOT = Pathname.new('.').expand_path
-
   Dir.glob(ROOT.join('api', '**', '*.rb')) { |f| require f }
 
   module Models
@@ -9,7 +8,7 @@ module API
 
   class App < Routes::Base
     get '/favicon.ico' do
-      send_file(ROOT.join('public', 'favicon.ico'))
+      send_file(ROOT.join('public', app_env.favicon))
     end
 
     use Routes::Users
