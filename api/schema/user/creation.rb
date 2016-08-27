d8c6512537230e56e5971cd1437a8b8adca4d607
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-08-26 22:32:01 -0700

    Refactor Schema::User::Creation

--- api/schema/user/creation.rb
+++ api/schema/user/creation.rb
@@ -2,26 +2,35 @@ module API
   module Schema
     module User
       class Creation
-        include Procto.call, Concord.new(:params, :repository)
+        include Procto.call, Concord.new(:params, :repository), Memoizable
 
         def call
-          dry = schema.call(params)
-          return dry unless dry.success?
-          Schema::Result.new(username_availability, dry.output)
+          Schema::Result.new(messages, dry_schema.output)
         end
 
         private
+
+        def messages
+          if dry_schema.success?
+            username_availability
+          else
+            dry_schema.messages
+          end
+        end
 
         def username_availability
           return {} if repository.available?(params)
           { username: ['has already been taken'] }
         end
 
-        def schema
-          Dry::Validation.Schema do
-            required(:username).filled(:str?, size?: 4..30)
-            required(:password).filled(:str?, size?: 6..255)
-          end
+        def dry_schema
+          schema =
+            Dry::Validation.Schema do
+              required(:username).filled(:str?, size?: 4..30)
+              required(:password).filled(:str?, size?: 6..255)
+            end
+
+          schema.call(params)
         end
       end
     end
