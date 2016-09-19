845ec4976e653467d4c35909e349a606b6ec0e64
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-09-18 21:54:37 -0700

    Augment specs for Builders::User

--- spec/api/builders/user_spec.rb
+++ spec/api/builders/user_spec.rb
@@ -1,4 +1,10 @@
 describe API::Builders::User, :db do
+  before do
+    allow(Sequel)
+      .to receive(:connect)
+      .and_return(Sequel.mock(host: 'postgres'))
+  end
+
   it 'has the expected VALIDATION_CLASS' do
     expect(described_class::VALIDATION_CLASS).to eql(API::Schema::User::Creation)
   end
@@ -26,12 +32,33 @@ describe API::Builders::User, :db do
     )
   end
 
-  it 'fails on authentication', :skip do # TODO implement HTTP stub
-    params = { username: 'dummy-username', password: 'dummy-password' }
-    expect(described_class.call(params, repository)).to eql(
-      errors: {
-        credentials: 'Authentication failed'
-      }
-    )
+  context 'failed authentication' do
+    let(:msgs) { { credentials: 'Bad credentials!' } }
+
+    before do
+      stub_const(
+        'API::Schema::User::MFPAuth',
+        class_double(API::Schema::User::MFPAuth, call: API::Schema::Result.new(msgs, {}))
+      )
+    end
+
+    it 'returns errors' do
+      params = { username: 'dummy-username', password: 'dummy-password' }
+      expect(described_class.call(params, repository)).to eql(errors: msgs)
+    end
+  end
+
+  context 'successful validation' do
+    before do
+      fake_result = API::Schema::Result.new({}, {})
+      fake_auth   = class_double(API::Schema::User::MFPAuth, call: fake_result)
+      stub_const('API::Schema::User::MFPAuth', fake_auth)
+    end
+
+    it 'returns errors' do
+      params = { username: 'dummy-username', password: 'dummy-password' }
+      expect_any_instance_of(described_class::MAPPER_CLASS).to receive(:create).once
+      described_class.call(params, repository)
+    end
   end
 end
